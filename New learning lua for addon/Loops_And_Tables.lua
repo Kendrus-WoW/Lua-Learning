7275e13e3d91669dbfa5d7601a80c614839629b7
Kendrus-WoW <180138469+Kendrus-WoW@users.noreply.github.com>
2025-04-17 05:16:34 -0300

    Lopps and table 100% functional

--- New learning lua for addon/Loops_And_Tables.lua
+++ New learning lua for addon/Loops_And_Tables.lua
@@ -1,14 +1,16 @@
-local function personagems_dados(nivel, name, class)
-    return "o " .. name .. " é um " .. class .. " de nivel " .. nivel ..""
+local function personagens_dados(nome, classe, nivel)
+    return "o seu personagem " .. nome .. " é do nivel " .. nivel .. " e o sua classe é " .. classe .. ""
 end
 
-local personagens = {
-     {name = "medivh", class = "Mago", nivel = 50},
-     {name = "Aragorn", class = "Ranger", nivel = 45},
-     {name = "Legolas", class = "Hunter", nivel = 60},
+local tabelinha = {
+    {nome = "Aragorn", nivel = 60, classe = "guerreiro"},
+    {nome = "legolas", nivel = 50, classe = "Arqueiro"},
+    {nome = "Elwin", nivel = 45, classe = "Sacerdotista"},
+    {nome = "Gimmli",nivel = 55, classe = "Guerreiro"},
 }
 
-for i, p in ipairs(personagens) do
-    local descricao =   personagems_dados(p.nivel, p.name, p.class)
+for i, p in ipairs(tabelinha) do
+    local descricao = personagens_dados(p.nome, p.nivel, p.classe)
     print(descricao)
 end
+   
