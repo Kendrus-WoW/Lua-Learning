d9e25b7311323829961cf5509f95ae12cbac8b6d
Kendrus-WoW <180138469+Kendrus-WoW@users.noreply.github.com>
2025-04-19 06:08:03 -0300

    more complex

--- New learning lua for addon/Loops_tables_and_status.lua
+++ New learning lua for addon/Loops_tables_and_status.lua
@@ -1,26 +1,36 @@
-local function personagens(classe, nome, nivel)
-    local status = ""
+local function personagens(nome,level,classe)
+      local nivel_status = ""   
 
-    if nivel >= 50 then  
-       status = " - já pode fazer raid !"
-    else
-        status = " - ainda não pode fazer raid ! "
-    end
-
-    return "a classe é " .. classe .. " e o nome é " .. nome .. " de nivel " .. nivel .. " e ele " .. status
-
+if level == 60 then
+   nivel_status = " // e voce esta no nivel maximo ! "
+else
+    nivel_status = " // voce ainda precisa upar para chegar ao nivel maximo !"
+    
 end
 
+local class_status
+if classe == "arqueiro" then
+    class_status = " -- o Arqueiro é uma classe mortal a distancia !"
+elseif classe == "Ranger" then
+    class_status = " -- a classe ranger é muito versatil "
+elseif classe == "Mago" then
+    class_status = " -- O mago é muito poderoso "
+else
+    class_status = "ERROR"
+end
+
+
+    return "o personagem " .. nome .. " é de nivel " .. level .." da classe " .. classe .. "" .. nivel_status .. class_status 
+
+end 
+
 local tabela = {
-    {classe = "guerreiro", nome = "aragorn", nivel = 40},
-    {classe = "mago", nome = "Etel", nivel = 55},
-    {classe = "Arqueiro", nome = "Rexxar", nivel = 65},
+    {nome = "Legolas", level = 60 , classe = "arqueiro"},
+    {nome = "Aragorn", level = 55 , classe = "Ranger"},
+    {nome = "Gandalf", level = 60, classe = "Mago"},
 }
 
 for i, p in ipairs(tabela) do
-local descricao = personagens(p.classe, p.nome, p.nivel)
+    local descricao = personagens(p.nome, p.level, p.classe)
     print(descricao)
 end
-
-local teste_1 = personagens("espartano", "maximus", 999)
-print(teste_1)
